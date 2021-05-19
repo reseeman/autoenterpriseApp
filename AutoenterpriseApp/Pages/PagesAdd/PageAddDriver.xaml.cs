bba35650b2c7c89423174cd49602848d0b9ae2df
SplashEnergy <46820987+SplashEnergy@users.noreply.github.com>
2021-05-19 10:50:03 +0300

    pageAddDriver

--- AutoenterpriseApp/Pages/PagesAdd/PageAddDriver.xaml.cs
+++ AutoenterpriseApp/Pages/PagesAdd/PageAddDriver.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using AutoenterpriseApp.DataBaseFiles;
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,11 +14,51 @@ namespace AutoenterpriseApp.Pages.PagesAdd
         public PageAddDriver()
         {
             InitializeComponent();
+
+            CmbBrigadeChoise.SelectedValuePath = "idBrigade";
+            CmbBrigadeChoise.DisplayMemberPath = "nameBrigade";
+            CmbBrigadeChoise.ItemsSource = OdbConnectHelper.entObj.brigades.ToList();
         }
 
         private void buttonBack(object sender, RoutedEventArgs e)
         {
             FrameApp.frmObj.GoBack();
+        }
+
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAddDriver(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //MessageBox.Show(CmbBrigadeChoise.SelectedValue.ToString());
+                drivers drvObj = new drivers()
+                {
+                    nameDriver = TbxFIO.Text,
+                    idBrigade = Convert.ToInt32(CmbBrigadeChoise.SelectedValue)
+                };
+
+                OdbConnectHelper.entObj.drivers.Add(drvObj);
+                OdbConnectHelper.entObj.SaveChanges();
+
+                MessageBox.Show("Водитель " + drvObj.nameDriver + " успешно добавлен!",
+                                "Уведомление",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Information
+                                );
+                FrameApp.frmObj.GoBack();
+
+                //SET IDENTITY_INSERT MyTable ON - при ошибке IDENTIY_OFF
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Критическая работа с приложением: " + ex.Message.ToString(),
+                    "Уведомление",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                    );
+            }
         }
     }
 }
